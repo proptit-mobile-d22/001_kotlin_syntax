8a5189f6b3ae859d7b446159c3959b24b14c7eca
gxdumplingg <hiengiangg@gmail.com>
2024-07-07 22:02:32 +0700

    add: homework

---
+++ Bai2.kt
@@ -0,0 +1,29 @@
+import kotlin.random.Random
+
+const val max_element1 = 1000005;
+fun solve(n : Int){
+    println(n)
+    if(n==1){
+        println("1")
+    }
+    else if (n==2 || n==3){
+        println("No Solution")
+        return
+    }
+    show(n)
+}
+fun show (n : Int){
+    val arr = mutableListOf<Int>()
+    for(i in 1 .. n step 2){
+        arr.add(i)
+    }
+    for(i in 2 .. n step 2){
+        arr.add(i)
+    }
+    println(arr)
+}
+
+fun main() {
+    val n = Random.nextInt(1, max_element1)
+    solve(n)
+}
